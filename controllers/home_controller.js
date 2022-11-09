8a75b11036d4178b2d216fbfcf90122b991f6b0e
Utkarsh Gupta <utkarshgupta04092003@gmail.com>
2022-11-10 00:33:01 +0530

    setup complete for show all posts on home.ejs

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,12 +1,24 @@
+const Post = require('../models/post');
+
 module.exports.home = function(req,res){
 
-    console.log(req.cookies);
+    // console.log(req.cookies);
+    // res.cookie('user_id',25);
 
-    res.cookie('user_id',25);
+    Post.find({},function(err,posts){
+        if(err){
+            console.log('Error in fetching all post data from db');
+            return;
+        }
 
-    console.log('Home controller!');
-    return res.render('./home',{
-        title: "Home | Codeial"
-    });
+        console.log('Home controller!');
+        return res.render('./home',{
+            title: "Home | Codeial",
+            posts: posts 
+        });
+
+    })
+
+    
 }
 
