341ecfa7bf08564a60c7dae2bff0fc00c66b3c0b
Utkarsh Gupta <utkarshgupta04092003@gmail.com>
2022-11-10 20:56:16 +0530

    check client side and router side authentication for creating post

--- routes/posts.js
+++ routes/posts.js
@@ -1,11 +1,12 @@
 // route for users
 const express = require('express');
 const router = express.Router();
+const passport = require('passport');
 
 
 const postsController = require('../controllers/posts_controller');
 
-router.post('/create',postsController.create);
+router.post('/create',passport.checkAuthentication,postsController.create);
 
 
 module.exports = router;
